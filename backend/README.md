13bf153345c8d3eceed5f266d010d2b477398843
tiffany <ting11222001@gmail.com>
2024-03-09 13:17:37 +1030

    Add register and login routes.
    - Add register logic in auth controller.
    - Add register route from authRoute to index.
    - Add a backend README.md

---
+++ backend/README.md
@@ -0,0 +1,25 @@
+# Backend
+
+Include step-by-step instructions on how to install and set up the backend project. This may include dependencies, environment setup, etc.
+
+## Installation
+
+```bash
+# Example installation command
+npm install
+```
+
+## Run the Server
+
+```bash
+# For Development
+npm run start-dev
+
+# For Testing the Frontend
+npm run start
+```
+
+## Postman
+
+### Post (Register)
+Use raw Body in JSON format to post a dummy user. 
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./Routes/auth.js";
 
 dotenv.config()
 
@@ -29,11 +30,11 @@ const connectDB = async () => {
   }
 }
 
-
 // middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
+app.use('/api/v1/auth', authRoute) // domain/api/v1/auth/register
 
 app.listen(port, () => {
   connectDB()
