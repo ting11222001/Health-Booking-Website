e97682b7e47e0229731f1765d2b49e4745ebd59f
tiffany <ting11222001@gmail.com>
2024-03-11 09:31:03 +1030

    Setup Layout.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import './app.css'
+import './App.css'
+import Layout from "./layout/Layout"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-[20px] text-blue-600">Hello!</h1>
-    </>
+    <Layout />
   )
 }
 
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,8 @@
+
+const Routers = () => {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
