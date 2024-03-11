86781ee9e5ea1f6dc035da7e5d11cf241032a9cc
tiffany <ting11222001@gmail.com>
2024-03-11 11:42:54 +1030

    Create navigation on the right.
    - Show menu icon on small screens.
    - Make the header sticky when the screen is scrolled down.
    - Add background to the header.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react"
 import logo from "../../assets/images/logo.png"
 import { NavLink, Link } from "react-router-dom"
+import userImg from "../../assets/images/avatar-icon.png"
+import { BiMenu } from "react-icons/bi"
 
 const navLinks = [
   {
@@ -22,8 +24,30 @@ const navLinks = [
 ]
 
 const Header = () => {
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('stick__header')
+      } else {
+        headerRef.current.classList.remove('stick__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleStickyHeader()
+
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  })
+
   return (
-    <header className="header flex items-center">
+    <header
+      className="header flex items-center"
+      ref={headerRef}
+    >
       <div className="container">
         <div className="flex items-center justify-between">
           {/* === logo === */}
@@ -48,6 +72,27 @@ const Header = () => {
               }
             </ul>
           </div>
+
+          {/* === nav right === */}
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to='/'>
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img src={userImg} className="w-full rounded-full" alt="" />
+                </figure>
+              </Link>
+            </div>
+          </div>
+
+          <Link to="/login">
+            <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">Login</button>
+          </Link>
+
+          <span className="md:hidden">
+            <BiMenu className="w-6 h-6 cursor-pointer" />
+          </span>
+
+          {/* WIP */}
         </div>
       </div>
     </header>
