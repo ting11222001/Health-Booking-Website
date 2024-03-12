cb3c51c391ff3cdad229fe442bdd841df0e37702
tiffany <ting11222001@gmail.com>
2024-03-12 10:44:49 +1030

    Add faq section in the home page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,8 @@ import featureImg from "../assets/images/feature-img.png"
 import videoImg from "../assets/images/video-icon.png"
 import avatarIcon from "../assets/images/avatar-icon.png"
 import DoctorList from "../components/Doctors/DoctorList"
+import faqImg from "../assets/images/faq-img.png"
+import FaqList from "../components/Faq/FaqList"
 
 const Home = () => {
   return (
@@ -214,6 +216,25 @@ const Home = () => {
           </div>
 
           <DoctorList />
+        </div>
+      </section>
+
+      {/* === faq ection === */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most uestions from our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+
         </div>
       </section>
     </>
