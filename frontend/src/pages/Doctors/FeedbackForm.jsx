1c2953a36ab5cb95fa24862a22f0f3bd2c8f5015
tiffany <ting11222001@gmail.com>
2024-03-13 17:41:07 +1030

    Add more details - Inside the feedback form in the feedback tab in Doctor Detail page. - Add rating starts and comments text area.

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -5,6 +5,14 @@ import { AiFillStar } from "react-icons/ai"
 const FeedbackForm = () => {
   const [rating, setRating] = useState(0)
   const [hover, setHover] = useState(0)
+  const [reviewText, setReviewText] = useState("")
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault()
+
+    // will use api here later
+  }
+
   return (
     <form action="">
       <div>
@@ -41,6 +49,28 @@ const FeedbackForm = () => {
           })}
         </div>
       </div>
+
+      <div className="mt-[30px]">
+        <h3 className="text-headingColor text-[16px] leading-6 fond-semibold mb-4 mt-0">
+          Share your feedback or suggestions
+        </h3>
+
+        <textarea
+          className="border border-solid border-[#0066ff34] 
+          focus:outline outline-primaryColor w-full px-4 py-3 rounded-md"
+          rows="5"
+          placeholder="Write your comments"
+          onChange={(e) => setReviewText(e.target.value)}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="btn"
+        onClick={handleSubmitReview}
+      >
+        Submit Feedback
+      </button>
     </form>
   )
 }
