79f66cacdfdcce86857d13b9aa7427e4fe3c904f
tiffany <ting11222001@gmail.com>
2024-03-13 17:56:31 +1030

    Add SidePanel i.e. the price, time and book on the right side of Doctor Detail page.

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from "../../assets/images/doctor-img02.png"
 import starIcon from "../../assets/images/Star.png"
 import DoctorAbout from "./DoctorAbout"
 import Feedback from "./Feedback"
+import SidePanel from "./SidePanel"
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState('about')
@@ -73,11 +74,13 @@ const DoctorDetails = () => {
               {
                 tab === "feedback" && <Feedback />
               }
-
             </div>
           </div>
 
-          <div></div>
+          {/* === Right content === */}
+          <div>
+            <SidePanel />
+          </div>
         </div>
       </div>
     </section>
