4bc77f9177e9579e1b05794a3a5677a3775bb930
tiffany <ting11222001@gmail.com>
2024-03-16 15:47:39 +1030

    Create a custom hook, useFetchData.
    - Create style, and loading animations in MyAccount page.
    - Show error message on screen when not authorized to see the page.

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -3,10 +3,21 @@ import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
 import MyBookings from "./MyBookings"
 import Profile from "./Profile"
+import useGetProfile from "../../hooks/useFetchData"
+import { BASE_URL } from "../../config"
+import Loading from "../../components/Loading/Loading"
+import Error from "../../components/Error/Error"
 
 const MyAccount = () => {
   const { dispatch } = useContext(AuthContext)
   const [tab, setTab] = useState('bookings')
+
+  const {
+    data: userData,
+    loading,
+    error
+  } = useGetProfile(`${BASE_URL}/users/profile/me`)
+  console.log("userData: ", userData)
 
   const handleLogOut = () => {
     localStorage.removeItem('user')
@@ -19,56 +30,66 @@ const MyAccount = () => {
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
-        <div className="grid md:grid-cols-3 gap-10">
-          <div className="pb-[50px] px-[30px] rounded-md">
-            <div className="flex items-center justify-center">
-              <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
-                <img src={userImg} alt="" className="w-full h-full rounded-full" />
-              </figure>
-            </div>
+        {
+          loading && <Loading />
+        }
 
-            <div className="text-center mt-4">
-              <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">Wayne Collins</h3>
-              <p className="text-textColor text-[15px] leading-6 font-medium">waynes@gmail.com</p>
-              <p className="text-textColor text-[15px] leading-6 font-medium">Blood TYpe:
-                <span className="ml-2 text-headingColor text-[22px] leading-8">O-</span>
-              </p>
-            </div>
+        {error && <Error errorMessage={error} />}
 
-            <div className="mt-[50px] md:mt-[100px]">
-              <button
-                className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
-                onClick={handleLogOut}
-              >
-                Log out
-              </button>
-              <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
-                Delete Account
-              </button>
-            </div>
-          </div>
+        {
+          !loading && !error && (
+            <div className="grid md:grid-cols-3 gap-10">
+              <div className="pb-[50px] px-[30px] rounded-md">
+                <div className="flex items-center justify-center">
+                  <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+                    <img src={userImg} alt="" className="w-full h-full rounded-full" />
+                  </figure>
+                </div>
 
-          <div className="md:col-span-2 md:px-[30px]">
-            <div>
-              <button
-                onClick={() => setTab('bookings')}
-                className={`${tab === 'bookings' && 'bg-primaryColor text-white font-normal'} p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
-                My Bookings
-              </button>
-              <button
-                onClick={() => setTab('settings')}
-                className={`${tab === 'settings' && 'bg-primaryColor text-white font-normal'} p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
-                Profile Settings
-              </button>
+                <div className="text-center mt-4">
+                  <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">Wayne Collins</h3>
+                  <p className="text-textColor text-[15px] leading-6 font-medium">waynes@gmail.com</p>
+                  <p className="text-textColor text-[15px] leading-6 font-medium">Blood TYpe:
+                    <span className="ml-2 text-headingColor text-[22px] leading-8">O-</span>
+                  </p>
+                </div>
+
+                <div className="mt-[50px] md:mt-[100px]">
+                  <button
+                    className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+                    onClick={handleLogOut}
+                  >
+                    Log out
+                  </button>
+                  <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+                    Delete Account
+                  </button>
+                </div>
+              </div>
+
+              <div className="md:col-span-2 md:px-[30px]">
+                <div>
+                  <button
+                    onClick={() => setTab('bookings')}
+                    className={`${tab === 'bookings' && 'bg-primaryColor text-white font-normal'} p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
+                    My Bookings
+                  </button>
+                  <button
+                    onClick={() => setTab('settings')}
+                    className={`${tab === 'settings' && 'bg-primaryColor text-white font-normal'} p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
+                    Profile Settings
+                  </button>
+                </div>
+                {
+                  tab === "bookings" && <MyBookings />
+                }
+                {
+                  tab === "settings" && <Profile />
+                }
+              </div>
             </div>
-            {
-              tab === "bookings" && <MyBookings />
-            }
-            {
-              tab === "settings" && <Profile />
-            }
-          </div>
-        </div>
+          )
+        }
       </div>
     </section>
   )
