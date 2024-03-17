df683554b50db5b73ce4abc19a8931c037f11003
tiffany <ting11222001@gmail.com>
2024-03-17 14:34:40 +1030

    Add Doctor's profile dashboard page.
    - Add content in the overview tab.

---
+++ frontend/src/Dashboard/doctor-account/Tabs.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import { useContext } from 'react'
+import { BiMenu } from 'react-icons/bi'
+import { AuthContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+const Tabs = ({ tab, setTab }) => {
+  const navigate = useNavigate()
+  const { dispatch } = useContext(AuthContext)
+
+  const handleLogOut = () => {
+    localStorage.removeItem('user')
+
+    dispatch({
+      type: "LOGOUT",
+    })
+    navigate('/')
+  }
+
+
+  return (
+    <div>
+      <span className='lg:hidden'>
+        <BiMenu className='w-6 h-6 cursor-pointer' />
+      </span>
+
+      {/* === left panel (only shown on big screen) === */}
+      <div className='hidden lg:flex flex-col p-[30px] bg-white shadow-panelShadow items-center h-max rounded-md'>
+        <button
+          onClick={() => setTab('overview')}
+          className={`
+          ${tab === "overview"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+            } w-full btn mt-0 rounded-md`}>
+          Overview
+        </button>
+
+        <button
+          onClick={() => setTab('appointments')}
+          className={`
+          ${tab === "appointments"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+            } w-full btn mt-0 rounded-md`}>
+          Appointments
+        </button>
+
+        <button
+          onClick={() => setTab('settings')}
+          className={`
+          ${tab === "settings"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+            } w-full btn mt-0 rounded-md`}>
+          Profile
+        </button>
+
+        <div className="mt-[100px] w-full">
+          <button
+            className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+            onClick={handleLogOut}
+          >
+            Log out
+          </button>
+          <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+            Delete Account
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tabs
--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,13 +1,13 @@
 import { formatDate } from "../../../src/utils/formatDate"
 
-const DoctorAbout = () => {
+const DoctorAbout = ({ name, about, qualifications, experiences }) => {
   return (
     <div>
       <div>
         <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
           About of
           <span className="text-irisBlueColor font-bold text-[24px] leading-9">
-            John Williams
+            {name}
           </span>
         </h3>
         <p className="text__para">
