00a5dfcea992b4b190c6d9293f3c630b4d67b10e
tiffany <ting11222001@gmail.com>
2024-03-18 18:15:42 +1030

    Update CheckoutSuccess page.

--- frontend/src/pages/CheckoutSuccess.jsx
+++ frontend/src/pages/CheckoutSuccess.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import { FaCheckCircle } from "react-icons/fa";
+
 
 const CheckoutSuccess = () => {
   return (
     <div className="bg-gray-100 h-screen">
       <div className="bg-white p-6 md:mx-auto">
         <div className="text-center">
+          <FaCheckCircle className="text-green-600 w-16 h-16 mx-auto my-6" />
           <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">
             Payment done!
           </h3>
@@ -15,7 +18,7 @@ const CheckoutSuccess = () => {
           <div className="py-10 text-center">
             <Link
               to="/home"
-              className="px-12 bh-buttonBgColor text-white font-semibold py-3"
+              className="px-12 bg-primaryColor text-white font-semibold py-3"
             >
               Go Back To Home
             </Link>
