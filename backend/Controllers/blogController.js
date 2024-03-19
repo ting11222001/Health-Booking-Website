a01e0a7af45a5462dafc9e7ec1e224a411e5aa44
tiffany <ting11222001@gmail.com>
2024-03-19 13:20:54 +1030

    Add getSingleBlog and getAllBlog APIs.
    - getAllBlog add "show only published ones" and "query".

--- backend/Controllers/blogController.js
+++ backend/Controllers/blogController.js
@@ -2,7 +2,7 @@ import Blog from "../Models/BlogSchema.js"
 import Doctor from "../Models/DoctorSchema.js"
 
 export const createBlog = async (req, res) => {
-  const id = req.params.id
+  const id = req.params.doctorId // "doctorId": depends on the id variable passed from the routes
   const newBlog = new Blog(req.body)
 
   try {
@@ -25,6 +25,61 @@ export const createBlog = async (req, res) => {
     res.status(500).json({
       success: false,
       message: error.message
+    })
+  }
+}
+
+export const getAllBlog = async (req, res) => {
+  try {
+
+    const { query } = req.query
+    let blogs
+
+    if (query) {
+      blogs = await Blog.find({
+        isPublished: 'published',
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { content: { $regex: query, $options: "i" } }
+        ]
+      })
+    } else {
+      blogs = await Blog.find({ isPublished: 'published' })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'All Blogs found',
+      data: blogs
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({
+      success: false,
+      message: 'No Blogs found'
+    })
+  }
+}
+
+export const getSingleBlog = async (req, res) => {
+  const id = req.params.id
+
+  try {
+
+    const singleBlog = await Blog.findById(id)
+
+    res.status(200).json({
+      success: true,
+      message: 'Blog successfully found',
+      data: singleBlog
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({
+      success: false,
+      message: 'No Blog found'
     })
   }
 }
--- backend/Routes/blog.js
+++ backend/Routes/blog.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { createBlog } from "../Controllers/blogController.js"
-import { authenticate, restrict } from "../auth/verifyToken.js"
+import { createBlog, getAllBlog, getSingleBlog } from "../Controllers/blogController.js"
 
 const router = express.Router()
 
-// router.post('/:id', authenticate, restrict(['doctor']), createBlog)
-router.post('/:id', createBlog)
+router.post('/:doctorId', createBlog)
+router.get('/', getAllBlog)
+router.get('/:id', getSingleBlog)
 
 export default router
