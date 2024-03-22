cd31cb5c137649351009f81d3817e562ba0ca772
tiffany <ting11222001@gmail.com>
2024-03-22 20:43:20 +1030

    Recover the original way of doing CORS.

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,7 @@ mongoose.set('strictQuery', false)
 // middleware
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors(corsOptions))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use('/api/v1/auth', authRoute) // e.g. domain/api/v1/auth/register
 app.use('/api/v1/users', userRoute) // e.g. domain/api/v1/users/
 app.use('/api/v1/doctors', doctorRoute) // e.g. domain/api/v1/doctors/
