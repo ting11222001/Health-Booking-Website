9df5bfec95cf94f6949adbd0a0aa7d4a9feac1fb
tiffany <ting11222001@gmail.com>
2024-03-22 21:11:22 +1030

    Try the new vercel.json and cors

--- backend/index.js
+++ backend/index.js
@@ -31,12 +31,9 @@ mongoose.set('strictQuery', false)
 // middleware
 app.use(express.json())
 app.use(cookieParser())
-// Enable CORS for all origins and set the necessary headers
+// Enable CORS for all origins
 app.use(cors({
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: true
 }))
 app.use('/api/v1/auth', authRoute) // e.g. domain/api/v1/auth/register
 app.use('/api/v1/users', userRoute) // e.g. domain/api/v1/users/
