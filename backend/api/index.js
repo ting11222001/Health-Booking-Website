3d7a6571a85ac8401b01e69a8b24ad48f08a39ce
tiffany <ting11222001@gmail.com>
2024-03-26 10:57:17 +1030

    fix seed data schema import

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,10 +11,10 @@ import bookingRoute from "../Routes/booking.js"
 import blogRoute from "../Routes/blog.js"
 import feedbackRoute from "../Routes/feedback.js"
 // For seed data
-// import Doctor from "./Models/DoctorSchema.js"
-// import { doctors } from "./seed/doctors.js"
-// import User from "./Models/UserSchema.js"
-// import { users } from "./seed/users.js"
+import Doctor from "../Models/DoctorSchema.js"
+import { doctors } from "../seed/doctors.js"
+import User from "../Models/UserSchema.js"
+import { users } from "../seed/users.js"
 
 dotenv.config()
 
@@ -26,27 +26,7 @@ app.get('/', (req, res) => {
   res.send("Api is working")
 })
 
-// Enable CORS for all origins 
-// because this didn't work: app.use(cors(corsOptions));
-const whitelist = [
-  '*'
-];
-
-// Below Middleware has this specific execution order
-// app.use((req, res, next) => {
-//   const origin = req.get('referer');
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === 'OPTIONS') res.sendStatus(200);
-//   else next();
-// });
-
+// Enable CORS for all origins
 app.use(cors())
 
 app.use(express.json())
@@ -64,38 +44,40 @@ app.use('/api/v1/feedbacks', feedbackRoute) // e.g. domain/api/v1/feedbacks/
 mongoose.set('strictQuery', false)
 
 // Database connection
-/* 
-  // Drop collections and Seed data
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => app.listen(port, () => console.log("Server is running on port: " + port)))
-    .then(() => {
-      // Drop all collections
-      try {
-        const collections = mongoose.connection.collections;
 
-        for (const key in collections) {
-          collections[key].drop();
-          console.log(`Collection ${key} dropped.`);
-        }
+// Drop collections and Seed data
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => app.listen(port, () => console.log("Server is running on port: " + port)))
+  .then(() => {
+    // Drop all collections
+    try {
+      const collections = mongoose.connection.collections;
 
-        console.log("All collections dropped successfully!");
-      } catch (error) {
-        console.error("Error dropping collections:", error);
+      for (const key in collections) {
+        collections[key].drop();
+        console.log(`Collection ${key} dropped.`);
       }
-    })
-    .then(() => {
-      // seed the database
-      Doctor.insertMany(doctors)
-      console.log("Seeded Doctors...")
-      User.insertMany(users)
-      console.log("Seeded Users...")
-    })
-    .catch((error) => console.log(`${error}: connection failed`))
-*/
+
+      console.log("All collections dropped successfully!");
+    } catch (error) {
+      console.error("Error dropping collections:", error);
+    }
+  })
+  .then(() => {
+    // seed the database
+    Doctor.insertMany(doctors)
+    console.log("Seeded Doctors...")
+    User.insertMany(users)
+    console.log("Seeded Users...")
+  })
+  .catch((error) => console.log(`${error}: connection failed`))
+
 
 // Database connection with no drop collections and seed data)
+/*
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => app.listen(port, () => console.log("Server is running on port: " + port)))
   .catch((error) => console.log(`${error}: connection failed`))
+*/
